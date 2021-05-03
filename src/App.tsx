57d7b3243bf072af0f61bce38975a12e9f007d47
Ayush Mainali <ayushmainali11@gmail.com>
2021-05-03 21:18:26 +0545

    Initialized new repository with basic color palatte added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello From React!!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  primary: "#FBC91b",
+  grey: "#F2F2F2",
+  textGrey: "rgba(0, 0, 0, 0.5)",
+  textBlack: "rgba(0, 0, 0, 0.82)"
+};
+
+
+export default colors;
