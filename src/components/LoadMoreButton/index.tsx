846fd67ea0a456b6dbb7a5ca6538834d7c9622d2
Ayush Mainali <ayushmainali11@gmail.com>
2021-05-03 22:40:39 +0545

    Added Open Sans font and Made skeleton of Load More Footer Button

---
+++ src/components/LoadMoreButton/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const _Button = styled.button`
+`;
+
+const LoadMoreButton = () => {
+    return (
+        <_Button>
+            Load More
+        </_Button>
+    )
+}
+
+export default LoadMoreButton
