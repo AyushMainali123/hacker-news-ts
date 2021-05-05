ce3d88231ead34c6a634762294f4977fbab5622d
Ayush Mainali <ayushmainali11@gmail.com>
2021-05-05 11:21:10 +0545

    Made Main Container to return Specific chunks of data

--- src/components/MainSectionMid/index.tsx
+++ src/components/MainSectionMid/index.tsx
@@ -6,13 +6,30 @@ const MainSectionMid = () => {
 
     const { state } = useContext(ArrayContext);
     console.log(state)
-    const { loading, data } = state;
+    const { loading, data, error } = state;
+
+    const returnMainContainerBody = () => {
+        if (loading === true) {
+            return <h1 className = "loading">Loading....</h1>
+        }
+        if (error.length > 0) {
+            return <h1 className = "error">{ error }</h1>
+        }
+        
+        const positionsArray = Array.from(Array(data.currentPosition + 1).keys())
+        const dataToBeReturned = positionsArray.map(position => data.chunksArray[position].map((item: number) => <div key={item} className="data">{item}</div>))
+        
+        return [
+            ...dataToBeReturned,
+        ]
+    }
+
 
     return (
         <SectionWrapper>
             {
-                loading === true ? <h1>Loading....</h1> : data.totalResponse.map((item: number) => <div key={item}>{ item }</div>)
-           }
+               returnMainContainerBody()
+            }
         </SectionWrapper>
     )
 }
