0ffc811f8fc04be9e02db7b3a5f01d1e65c9c485
AyushMainali123 <ayushmainali11@gmail.com>
2021-05-06 22:24:08 +0545

    Made Details Main Page to rendered Fetch Items

--- src/DetailsMain/index.tsx
+++ src/DetailsMain/index.tsx
@@ -1,13 +1,66 @@
-
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { fetchHackerAPIItemsFromID } from "src/utils/fetch-items-from-id";
+import { HackerNewsResponseItem } from "src/interfaces/HackerNewsResponseItem";
+import Spinner from "src/components/Spinner";
+import { Link } from "react-router-dom";
+import Button from "src/components/Button";
+import MainContainerWrapper from '../containers/MainContainerWrapper'
 interface IProps {
-    id: string
+  id: string;
 }
 
-const DetailsMain = ({id}: IProps) => {
+const StyledGoBackButton = styled(Button)`
+  padding: 10px 15px;
+  margin: 12px 0;
+`;
 
-    return <div>
-      {id}
-  </div>;
+const TopSection = styled.div``;
+const MidSection = styled.div``;
+
+const DetailsMain = ({ id }: IProps) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+  const [data, setData] = useState<HackerNewsResponseItem | null>(null);
+
+  useEffect(() => {
+    const fetchFromApi = async () => {
+      const result = await fetchHackerAPIItemsFromID(Number(id));
+      return result;
+    };
+    const apiCallerFunction = async () => {
+      try {
+        const result = await fetchFromApi();
+        setLoading(false);
+        setData(result.data);
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+      }
+    };
+    apiCallerFunction();
+  }, [id]);
+
+  const returnRequiredJSX = () => {
+    if (loading) {
+      return <Spinner />;
+    }
+    if (error.length > 0) {
+      return <div>{error}</div>;
+    }
+    return <div>{data !== null ? data.title : null}</div>;
+  };
+
+  return (
+    <MainContainerWrapper>
+      <TopSection>
+        <Link to="/">
+          <StyledGoBackButton variant="default">Go Back</StyledGoBackButton>
+        </Link>
+      </TopSection>
+      <MidSection>{returnRequiredJSX()}</MidSection>
+    </MainContainerWrapper>
+  );
 };
 
 export default DetailsMain;
