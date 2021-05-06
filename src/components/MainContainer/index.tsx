269765677e6984c83da0503b560079d94db58385
AyushMainali123 <ayushmainali11@gmail.com>
2021-05-06 22:23:20 +0545

    Made Details component to render detch datas

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -1,92 +1,88 @@
-import colors from "src/styles/colors";
-import styled from "styled-components";
 import MainSectionBottom from "../MainSectionBottom";
 import MainSectionMid from "../MainSectionMid";
 import MainSectionTop from "../MainSectionTop";
-import {useState,  useContext, useEffect} from 'react'
-import { ArrayContext } from '../../Context/HackerNewsResponseArrayContext'
-import { ActionType as ArrayActionType } from '../../actions/HackerNewsResponseArray'
-import { ActionType as ItemActionType } from '../../actions//HackerNewsResponseItems'
-import {ItemsContext} from '../../Context/HackerNewsResponseItemsContext'
+import { useState, useContext, useEffect } from "react";
+import { ArrayContext } from "../../Context/HackerNewsResponseArrayContext";
+import { ActionType as ArrayActionType } from "../../actions/HackerNewsResponseArray";
+import { ActionType as ItemActionType } from "../../actions//HackerNewsResponseItems";
+import { ItemsContext } from "../../Context/HackerNewsResponseItemsContext";
 import axios from "src/axios";
-import {fetchHackerAPIItemsFromID} from 'src/utils/fetch-items-from-id'
+import { fetchHackerAPIItemsFromID } from "src/utils/fetch-items-from-id";
+import MainContainerWrapper from "../../containers/MainContainerWrapper";
 
-const MainContainerWrapper = styled.div`
-  background: ${colors.white};
-  padding-left: 15px;
-  padding-right: 15px;
-  box-shadow: inset 0px 3px 8px rgba(0, 0, 0, 0.08);
-  height: calc(100vh - 150px);
-  overflow-y: auto;
-  overflow-x: hidden;
-  word-wrap: break-word;
-`;
-
-type ActiveValueTypes = "New" | "Past"
+type ActiveValueTypes = "New" | "Past";
 
 const MainContainer = () => {
   const [activeValue, setActiveValue] = useState<ActiveValueTypes>("New");
-  const {  dispatch: arrayDispatch, state: arrayState } = useContext(ArrayContext)
-  const { dispatch: itemsDispatch, state: itemsState } = useContext(ItemsContext)
-  
+  const { dispatch: arrayDispatch, state: arrayState } = useContext(
+    ArrayContext
+  );
+  const { dispatch: itemsDispatch, state: itemsState } = useContext(
+    ItemsContext
+  );
 
   // For Array of Ids
   useEffect(() => {
     const getHackerRankResponseArray = async (url: string) => {
       try {
-        const responseArray = await axios(url)
+        const responseArray = await axios(url);
         // When Successfully Fetched From Server
         arrayDispatch({
           type: ArrayActionType.FETCH_SUCCESS,
-          payload: responseArray
-        })
+          payload: responseArray,
+        });
         return responseArray;
-      }
-      catch (error) {
-
+      } catch (error) {
         // When Fetching is not successful
         arrayDispatch({
           type: ArrayActionType.FETCH_FAILURE,
           payload: {
-            message: error.message
-          }
-          })        
+            message: error.message,
+          },
+        });
       }
-    }
+    };
 
     const apiCallerFunction = async () => {
-      const response = await getHackerRankResponseArray('/newstories.json?print=pretty')    
+      const response = await getHackerRankResponseArray(
+        "/newstories.json?print=pretty"
+      );
       return response;
-    }
+    };
     apiCallerFunction();
-    
-      
-  }, [arrayDispatch, itemsDispatch])
+  }, [arrayDispatch, itemsDispatch]);
 
   // Getting Items from each Array
   useEffect(() => {
     const itemsApiCallerFunction = async () => {
       if (arrayState.data.totalResponse.length > 0) {
-        const promisesArray = arrayState.data.chunksArray[arrayState.data.currentPosition].map((id: number) => fetchHackerAPIItemsFromID(id))
+        const promisesArray = arrayState.data.chunksArray[
+          arrayState.data.currentPosition
+        ].map((id: number) => fetchHackerAPIItemsFromID(id));
         const resolvedResponse = await Promise.all(promisesArray);
-        const dataToBeDispatched = resolvedResponse.map(response => response.data)
+        const dataToBeDispatched = resolvedResponse.map(
+          (response) => response.data
+        );
         console.log(dataToBeDispatched);
         itemsDispatch({
           type: ItemActionType.FETCH_SUCCESS,
           payload: {
-            data: dataToBeDispatched
-          }
-        })
+            data: dataToBeDispatched,
+          },
+        });
         console.log(itemsState, arrayState);
-    }
-  }
-    itemsApiCallerFunction()
-  }, [arrayState])
+      }
+    };
+    itemsApiCallerFunction();
+  }, [arrayState]);
 
   return (
     <MainContainerWrapper>
       {/* Top Section */}
-      <MainSectionTop activeValue={activeValue} setActiveValue={ setActiveValue }/>
+      <MainSectionTop
+        activeValue={activeValue}
+        setActiveValue={setActiveValue}
+      />
 
       {/* Mid Section */}
       <MainSectionMid />
